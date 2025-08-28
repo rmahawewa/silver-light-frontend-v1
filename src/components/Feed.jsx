5455f719996018f3e6bec9648213bc6958b35ce9
rmahawewa <rmahawewa@yahoo.com>
2025-08-28 09:39:37 +0530

    use memoization to optimize the feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, memo, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ImageCard from "./ImageCard";
 import PostCard from "./PostCard";
 import { GetFeed, GetConnections } from "./UserFunctions/GetFeed";
+
+// Memoize the child components
+const MemoizedImageCard = memo(ImageCard);
+const MemoizedPostCard = memo(PostCard);
 
 const Feed = () => {
 	const dispatch = useDispatch();
@@ -12,28 +16,38 @@ const Feed = () => {
 	console.log(feedData);
 	console.log(postData);
 
-	useEffect(() => {
+	// Use useCallback to memoize the dispatch functions
+	const getFeedMemoized = useCallback(() => {
 		GetFeed(dispatch, category);
-		GetConnections(dispatch);
-	}, []);
+	}, [dispatch, category]);
 
-	if (!feedData) return;
-	if (feedData.length === 0)
+	const getConnectionsMemoized = useCallback(() => {
+		GetConnections(dispatch);
+	}, [dispatch]);
+
+	useEffect(() => {
+		getFeedMemoized();
+		getConnectionsMemoized();
+	}, [getFeedMemoized, getConnectionsMemoized]);
+
+	if (!feedData) return null; // Use null for no content
+	if (feedData.length === 0 && (!postData || postData.length === 0))
 		return <h1 className="flex justify-center my-10">Feed is empty</h1>;
 
 	return (
 		<>
-			{feedData != {} &&
-				feedData &&
+			{feedData &&
+				feedData.length > 0 &&
 				feedData.map((img) => (
 					<div key={img._id} className=" flex justify-center py-10">
-						<ImageCard key={img._id} imageId={img._id} />
+						<MemoizedImageCard imageId={img._id} />
 					</div>
 				))}
 			{postData &&
+				postData.length > 0 &&
 				postData.map((post) => (
 					<div key={post._id} className="flex justify-center py-10">
-						<PostCard key={post._id} postId={post._id} />
+						<MemoizedPostCard postId={post._id} />
 					</div>
 				))}
 		</>
