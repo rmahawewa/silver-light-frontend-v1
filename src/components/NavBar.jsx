83157a0c5e1767549add5f5a9ea68196dcf957d1
rmahawewa <rmahawewa@yahoo.com>
2025-09-20 16:01:07 +0530

    notifications

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -148,7 +148,7 @@ const NavBar = () => {
 								tabIndex={0}
 								className="dropdown-content menu bg-base-100 rounded-box z-1 w-75 p-2 shadow-sm"
 							>
-								{notifications &&
+								{notifications.length > 0 &&
 									notifications?.map((notification) => (
 										<li
 											key={notification?.notification_id}
